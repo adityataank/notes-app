b02317b51716d069e7f13d092bf8ef98c3819e55
adityataank <adityataank2000@gmail.com>
2024-09-06 13:55:37 +0530

    feat: Added request processing time in request logger

--- notes-app-server/internal/middleware/request-logger.go
+++ notes-app-server/internal/middleware/request-logger.go
@@ -3,14 +3,19 @@ package middleware
 import (
 	"fmt"
 	"net/http"
+	"time"
 
 	"github.com/adityataank/notes-app/pkg/logger"
 )
 
 func RequestLogger(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		startTime := time.Now()
+
 		var logRequest = fmt.Sprintf("%s %s", r.Method, r.URL)
 		logger.RequestLog(logRequest)
 		next.ServeHTTP(w, r)
+
+		fmt.Printf("Request processed in %s\n\n", time.Since(startTime)) // Time taken to process the request.
 	})
 }
