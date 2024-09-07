8734ae4d83fb3577fab84e0d2d7c513a288d35aa
adityataank <adityataank2000@gmail.com>
2024-09-07 12:41:44 +0530

    fix: changed the production API root url

--- notes-app-client/src/lib/constants.ts
+++ notes-app-client/src/lib/constants.ts
@@ -2,7 +2,7 @@ export const FREE_ROUTES = ["/sign-in", "/sign-up", "/reset-password"];
 
 export const API_ROOT_URL =
   import.meta.env.VITE_MODE === "production"
-    ? "https://notes-app-r64d.onrender.com/api/v1"
+    ? import.meta.env.VITE_PRODUCTION_API_ROOT_URL
     : "http://localhost:8000/api/v1";
 
 export const API_KEY = import.meta.env.VITE_API_KEY;
